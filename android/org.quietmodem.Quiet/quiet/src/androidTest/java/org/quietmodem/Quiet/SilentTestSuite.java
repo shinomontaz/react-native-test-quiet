822360a863b82b06803081388dc4bc67ce546beb
drybakov <shinomontaz@gmail.com>
2023-03-13 01:21:04 +0300

    fix: new version

--- android/org.quietmodem.Quiet/quiet/src/androidTest/java/org/quietmodem/Quiet/SilentTestSuite.java
+++
@@ -1,8 +0,0 @@
-package org.quietmodem.Quiet;
-
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-
-@RunWith(Suite.class)
-@Suite.SuiteClasses({AutoIPTest.class, FrameTest.class, DatagramTest.class})
-public class SilentTestSuite {}
--- android/src/main/java/com/testquiet/TestQuietModule.java
+++ android/src/main/java/com/testquiet/TestQuietModule.java
@@ -2,6 +2,8 @@ package com.testquiet;
 
 import androidx.annotation.NonNull;
 
+import android.Manifest;
+
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
@@ -16,6 +18,8 @@ import org.quietmodem.Quiet.*;
 @ReactModule(name = TestQuietModule.NAME)
 public class TestQuietModule extends ReactContextBaseJavaModule {
   public static final String NAME = "TestQuiet";
+
+  private final ReactApplicationContext reactContext;
 
   private static FrameTransmitter transmitter = null;
 
@@ -36,25 +40,21 @@ public class TestQuietModule extends ReactContextBaseJavaModule {
     final boolean hasPermission = ContextCompat.checkSelfPermission(this.reactContext, Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED;
     if (!hasPermission) {
       System.out.println( "audio record is not permitted here!" );
-      return
+      return;
     }
+
     try {
-      this.transmitterConfig = new FrameTransmitterConfig(
-          this,
-          "audible-7k-channel-0");
-      receiverConfig = new FrameReceiverConfig(
-          this,
-          "audible-7k-channel-0");
+      this.transmitter = new FrameTransmitter(
+        new FrameTransmitterConfig(
+        this.reactContext,
+        "audible-7k-channel-0")
+      );
+      this.transmitterConfig = ;
     } catch (IOException e) {
       e.printStackTrace();
-    }
-
-    try {
-        this.transmitter = new FrameTransmitter(transmitterConfig);
     } catch (ModemException e) {
-        e.printStackTrace();
+      e.printStackTrace();
     }
-
   }
 
   @ReactMethod
